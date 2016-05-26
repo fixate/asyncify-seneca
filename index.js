ef86a43ca97939b179bbc93b9d7acb80aaef32b2
Stan Bondi <stan@fixate.it>
2016-05-26 15:49:51 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+'use strict'
+const Promise = require('bluebird');
+const co = require('co');
+
+module.exports = asyncifySeneca;
+
+function asyncifySeneca(seneca) {
+  seneca.actAsync = Promise.promisify(seneca.act, { context: seneca });
+  seneca.addAsync = function addAsync(props, func) {
+    seneca.add(props, (args, done) => {
+      co.wrap(func)(args).then((r) => done(null, r)).catch(done);
+    })
+  };
+  return seneca;
+}
